2f89b14f75c80a71c2874f60b437e1d320d59d26
keetasan <keetasan7@gmail.com>
2020-12-20 11:47:01 +0900

    TODOリストを作成

--- webDesign/todo/main.js
+++ webDesign/todo/main.js
@@ -4,73 +4,68 @@ addNewTodo.addEventListener('click', () => {
   const valueNewTodo = document.querySelector('#value-new-todo').value;
   if (valueNewTodo != '') {
     const inCompleteTodo = document.querySelector('#incomplete-todo');
-    const inCompleteTodoNumber = inCompleteTodo.children.length + 1;
+    createTodo(inCompleteTodo, valueNewTodo);
     document.querySelector('#value-new-todo').value = '';
-
-    const li = document.createElement('li');
-
-    const input = document.createElement('input');
-    input.type = 'checkbox';
-    input.id = 'incomplete-check' + inCompleteTodoNumber;
-
-    const label = document.createElement('label');
-    label.setAttribute('for', `incomplete-check${inCompleteTodoNumber}`);
-    label.innerText = valueNewTodo;
-
-    li.appendChild(input);
-    li.appendChild(label);
-    inCompleteTodo.appendChild(li);
   } else {
     alert('登録したいTODOを入力してください');
   }
 });
 
-const checkboxs = document.querySelectorAll('input[type="checkbox"]');
-checkboxs.forEach((checkbox) => {
-  checkbox.addEventListener('click', () => {
-    console.log(checkbox.checked);
-    if (checkbox.checked) {
-      const todoText = checkbox.nextElementSibling.innerText;
-      const inCompleteTodo = document.querySelector('#incomplete-todo');
-      const moveTodo = checkbox.parentNode;
-      inCompleteTodo.removeChild(moveTodo);
-      const completeTodo = document.querySelector('#complete-todo');
-      const completeTodoNumber = completeTodo.children.length + 1;
-
-      const li = document.createElement('li');
-
-      const input = document.createElement('input');
-      input.type = 'checkbox';
-      input.id = 'complete-check' + completeTodoNumber;
-      input.checked = true;
-
-      const label = document.createElement('label');
-      label.setAttribute('for', `complete-check${completeTodoNumber}`);
-      label.innerText = todoText;
-
-      li.appendChild(input);
-      li.appendChild(label);
-      completeTodo.appendChild(li);
-      console.log(checkboxs);
-    } else if (!checkbox.checked) {
-      const todoText = checkbox.nextElementSibling.innerText;
-      const completeTodo = document.querySelector('#complete-todo');
-      console.log(completeTodo);
-      const moveTodo = checkbox.parentNode;
-      completeTodo.removeChild(moveTodo);
-      const inCompleteTodo = document.querySelector('#incomplete-todo');
-      const inCompleteTodoNumber = inCompleteTodo.children.length + 1;
-      const li = document.createElement('li');
-      const input = document.createElement('input');
-      input.type = 'checkbox';
-      input.id = 'incomplete-check' + inCompleteTodoNumber;
-      input.checked = false;
-      const label = document.createElement('label');
-      label.setAttribute('for', `incomplete-check${inCompleteTodoNumber}`);
-      label.innerText = todoText;
-      li.appendChild(input);
-      li.appendChild(label);
-      inCompleteTodo.appendChild(li);
+// const checkboxs = document.querySelectorAll('input[type="checkbox"]');
+// checkboxs.forEach((checkbox) => {
+//   checkbox.addEventListener('click', () => {
+//     const incompleteList = moveCheckbox();
+//   });
+// });
+function moveCheckbox() {
+  const checkboxs = document.querySelectorAll('input[type="checkbox"]');
+  const incompleteList = [];
+  const completeList = [];
+  for (let i = 0; i < checkboxs.length; i++) {
+    if (checkboxs[i].checked) {
+      completeList.push(checkboxs[i].nextElementSibling.innerText);
+    } else {
+      incompleteList.push(checkboxs[i].nextElementSibling.innerText);
     }
+  }
+  console.log(incompleteList);
+  const inCompleteTodo = document.querySelector('#incomplete-todo');
+  while (inCompleteTodo.firstChild) {
+    inCompleteTodo.removeChild(inCompleteTodo.firstChild);
+  }
+  const completeTodo = document.querySelector('#complete-todo');
+  while (completeTodo.firstChild) {
+    completeTodo.removeChild(completeTodo.firstChild);
+  }
+  for (let i = 0; i < incompleteList.length; i++) {
+    createTodo(inCompleteTodo, incompleteList[i]);
+  }
+  for (let i = 0; i < completeList.length; i++) {
+    createTodo(completeTodo, completeList[i]);
+  }
+}
+
+let todoNumber = 1;
+function createTodo(todo, todoContent) {
+  const li = document.createElement('li');
+
+  const input = document.createElement('input');
+  input.type = 'checkbox';
+  input.id = 'check' + todoNumber;
+  input.addEventListener('click', () => {
+    moveCheckbox();
   });
-});
+  if (todo.id == 'complete-todo') {
+    input.checked = true;
+  }
+
+  const label = document.createElement('label');
+  label.setAttribute('for', `check${todoNumber}`);
+  label.innerText = todoContent;
+
+  todoNumber++;
+
+  li.appendChild(input);
+  li.appendChild(label);
+  todo.appendChild(li);
+}
